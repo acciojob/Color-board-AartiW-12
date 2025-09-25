d594359e06bacf966d0878df6e81dc6a67318a2c
AartiW-12 <aartiwhaval12@gmail.com>
2025-09-25 10:23:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const allSquares = document.getElementById("block");
+
+for(let i=0;i<800;i++){
+	const sq = document.createElement("div");
+	sq.classList.add('square');
+
+	sq.addEventListener('mouseover' , ()=>{
+		const color = `hsl(${Math.random() * 360},70% , 50%)`;
+		sq.style.backgroundColor = color;
+	})
+
+	sq.addEventListener('mouseleave' , ()=>{
+			sq.style.backgroundColor = '#333'
+	})
+	allSquares.appendChild(sq);
+}
